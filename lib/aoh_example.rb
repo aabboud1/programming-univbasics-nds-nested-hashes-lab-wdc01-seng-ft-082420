c1a11aed296533f2f1c4ade030196e47c95d814b
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-10 18:44:55 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -37,6 +37,6 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  new = aoh[row][key] = {new_value}
+  new = aoh[row][key] = :new_value
   new
 end
